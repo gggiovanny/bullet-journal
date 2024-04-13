aba79bfa7965a00374273155e7304e259768b8ed
Giovanny González Baltazar <giovanny@mifiel.com>
2024-04-13 08:39:59 -0600

    adds tailwind plugin to expose colors as css vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import type { Config } from 'tailwindcss';
 
 const deepBlue = '#1A0933';
@@ -38,6 +40,25 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      });
+    },
+  ],
 };
 export default config;
