0ca1a8994079e4f926532fa6a8cc8b475b7f213f
Giovanny González Baltazar <giovanny@mifiel.com>
2024-04-15 22:54:59 -0600

    small style adjustments

--- src/app/app/components/BookmarkNav.tsx
+++ src/app/app/components/BookmarkNav.tsx
@@ -3,7 +3,7 @@ import { TiHomeOutline } from 'react-icons/ti';
 import BookmarkTab from './BookmarkTab';
 
 const bookmarks = [
-  { text: <TiHomeOutline />, color: '#C6CAE9' },
+  { text: <TiHomeOutline size={20} />, color: '#C6CAE9' },
   { text: 'personal', color: '#C6F0EE' },
   { text: 'por mes', color: '#CAF1BC' },
   { text: 'año en pixeles', color: '#F2D4B1' },
@@ -11,11 +11,11 @@ const bookmarks = [
   { text: 'creativo', color: '#FBCAE1' },
 ];
 
-type Props = {};
+type Props = JSX.IntrinsicElements['nav'] & {};
 
-export default function BookmarkNav({}: Props) {
+export default function BookmarkNav({ ...navProps }: Props) {
   return (
-    <nav>
+    <nav {...navProps}>
       <ul className="flex flex-col font-title text-sm font-normal text-text-color">
         {bookmarks.map(({ text, color }, index) => (
           <BookmarkTab
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -5,12 +5,11 @@ import BookmarkNav from './components/BookmarkNav';
 export default function PageRoot() {
   return (
     <main className="min-h-screen flex flex-col">
-      <h1 className="font-title text-9xl font-extrabold text-text-color-dim opacity-50 mt-[-40px] ml-[-10px]">
-        crear
-      </h1>
-
-      <div className="grid grid-flow-col grid-cols-8 flex-grow">
+      <div className="grid grid-flow-col grid-cols-8">
         <section className="col-span-7 h-full">
+          <h1 className="font-title text-9xl font-extrabold text-text-color-dim opacity-50 mt-[-40px] ml-[-10px]">
+            crear
+          </h1>
           <div className="text-center font-italic italic text-text-color-alt font-normal h-full text-sm">
             <div className="w-56 mx-auto flex flex-col justify-center align-middle gap-3 h-full">
               <p className="underline">Agenda Vacía</p>
@@ -19,7 +18,7 @@ export default function PageRoot() {
           </div>
         </section>
         <aside className="col-span-1">
-          <BookmarkNav />
+          <BookmarkNav className="mt-4" />
         </aside>
       </div>
     </main>
