4107952298a70656cf170f40563cda07609ce4ee
Giovanny González Baltazar <giovanny@mifiel.com>
2024-04-17 09:17:31 -0600

    adds checked state

--- src/app/app/components/BigIconCheckbox.tsx
+++ src/app/app/components/BigIconCheckbox.tsx
@@ -1,11 +1,31 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import { PiCheckBold } from 'react-icons/pi';
+
 type Props = {
   text: string;
   Icon: any;
 };
 
-export default function BigIconCheckbox({ text, Icon }: Props) {
+export default function BigIconCheckbox({ text, Icon: TargetIcon }: Props) {
+  const [checked, setChecked] = useState(false);
+
+  const handleClick = () => {
+    setChecked(!checked);
+  };
+
+  const Icon = checked ? PiCheckBold : TargetIcon;
   return (
-    <div className="rounded-2.5xl shadow-button-shadow text-melon bg-background-color w-20 h-20 flex flex-col justify-center items-center pb-2 px-3 pt-2">
+    <div
+      className={clsx(
+        'rounded-2.5xl text-melon bg-background-color w-20 h-20 flex flex-col justify-center items-center pb-2 px-3 pt-2',
+        {
+          'shadow-button-shadow': !checked,
+          'shadow-button-inner-shadow': checked,
+        }
+      )}
+      onClick={handleClick}
+    >
       <Icon size="40" />
       <span className="font-title font-extrabold text-xs text-center leading-[normal]">
         {text}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,7 @@ const config: Config = {
       },
       boxShadow: {
         'button-shadow': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+        'button-inner-shadow': 'inset 0 4px 8px 0 rgb(0 0 0 / 0.2)',
       },
       borderRadius: {
         '2.5xl': '1.25rem',
